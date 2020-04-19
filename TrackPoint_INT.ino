a5b76aa0e5ea25c0afc2ca302143b9508b8ed535
chris <gzerone@gmail.com>
2020-04-19 23:16:33 +0800

    comment for teensy 2.0 supporting

--- TrackPoint_INT.ino
+++ TrackPoint_INT.ino
@@ -1,15 +1,22 @@
+ #include <Mouse.h>
+
 /*
  * Created on 14/03/2014
  * Author: Cong Nguyen
  *
  * Using an Arduino Leonardo to interface with a TrackPoint
- * Pin 3 (int.0)	: CLOCK
- * Pin 2			: DATA
- * Pin 4			: RESET
+ * Pin 3 (int.0)	: CLOCK  SCL TP_6
+ * Pin 2			: DATA  SDA TP_1
+ * Pin 4			: RESET  TP_2
+ * 
+ * Using Teensy 2.0 
+ * PIN_D0 (5): CLOCK  SCL TP_6
+ * PIN_D1 (6): DATA  SDA TP_1
+ * PIN_D2 (7): RESET  TP_2
  */
 #include "TrackPoint.h"
 
-#define	CLOCK		3
+#define CLOCK		3
 #define DATA		2
 #define RESET		0
 #define CLOCK_INT	0
@@ -32,6 +39,7 @@ void loop()
 	if(trackpoint.reportAvailable()) {
 		TrackPoint::DataReport d = trackpoint.getStreamReport();
 		Mouse.move(d.x, -d.y, 0);
+		Serial.println(d.state % 16 % 7);  // mouse button value
 	} 
 	
 }
